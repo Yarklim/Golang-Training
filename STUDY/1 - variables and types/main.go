efdda83e05f2d461a10ea4581acc117086de8ba9
Yaroslav Klimas <yarklim@gmail.com>
2025-10-19 14:29:41 +0300

    UPDATE: variables and types

--- STUDY/1 - variables and types/main.go
+++ STUDY/1 - variables and types/main.go
@@ -6,17 +6,20 @@ import (
 )
 
 func main() {
-	textType := "Hello, Yar!"  // Присвоение значения в переменную
-	intType := 10  // Целое число нельзя изменить на дробное число
-	floatType := 0.5  // Дробное число нельзя изменить на целое число
+	textType := "Hello, Yar!" // Присвоение значения в переменную
+	intType := 10             // Целое число нельзя изменить на дробное число
+	floatType := 0.5          // Дробное число нельзя изменить на целое число
 	boolType := true
 
 	floatTypeResult := 2 * floatType  // Результат будет целым числом, но тип будет float64
-	intResult := int(floatTypeResult)  // Приведение к типу int
+	intResult := int(floatTypeResult) // Приведение к типу int
 
-	resDevision := intType / 3  // Результат будет 3, а не 3.33333. Если делитель и делимое целые числа, то результат всегда будет целым числом (целочисленное деление)
+	resDevision := intType / 3 // Результат будет 3, а не 3.33333. Если делитель и делимое целые числа, то результат всегда будет целым числом (целочисленное деление)
+	// Чтобы результат деления был правильным, делимое должно быть типа float64
 
-	fmt.Println(textType)  // Вывод с переносом
+	remainderFromDivision := intType % 3 // Остаток от деления. Здесь равен 1.
+
+	fmt.Println(textType) // Вывод с переносом
 	fmt.Println(intType)  // Вывод в одну строку
 	fmt.Println(floatType)
 	fmt.Println(boolType)
@@ -24,4 +27,5 @@ func main() {
 	fmt.Println("Тип floatTypeResult:", reflect.TypeOf(floatTypeResult))
 	fmt.Println("Тип intResult:", reflect.TypeOf(intResult))
 	fmt.Println(resDevision)
+	fmt.Println(remainderFromDivision)
 }
