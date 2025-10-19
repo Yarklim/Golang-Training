1f7fa4e6b792ab091a686f06833f8150bfc697f0
Yaroslav Klimas <yarklim@gmail.com>
2025-10-19 17:49:22 +0300

    UPDATE: tutorial 2_if-else

--- STUDY/2_if-else/main.go
+++ STUDY/2_if-else/main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	score := 11
+	score := 17
 	text1 := "You are the best!"
 	text2 := "Train more"
+	text3 := "You are the MEGA-BEST!"
 
-	if score >= 10 {
+	if score > 15 {
+		fmt.Println(text3)
+	} else if score > 10 {
 		fmt.Println(text1)
 	} else {
 		fmt.Println(text2)
