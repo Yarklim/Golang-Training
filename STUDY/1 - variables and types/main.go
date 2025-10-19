07b0556e0fb966194f3337ca2c4eb257d5788bf6
Yaroslav Klimas <yarklim@gmail.com>
2025-10-19 14:10:20 +0300

    UPDATE: main.go

---
+++ STUDY/1 - variables and types/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	textType := "Hello, Yar!"  // Присвоение значения в переменную
+	intType := 10  // Целое число нельзя изменить на дробное число
+	floatType := 0.5  // Дробное число нельзя изменить на целое число
+	boolType := true
+
+	floatTypeResult := 2 * floatType  // Результат будет целым числом, но тип будет float64
+	intResult := int(floatTypeResult)  // Приведение к типу int
+
+	resDevision := intType / 3  // Результат будет 3, а не 3.33333. Если делитель и делимое целые числа, то результат всегда будет целым числом (целочисленное деление)
+
+	fmt.Println(textType)  // Вывод с переносом
+	fmt.Println(intType)  // Вывод в одну строку
+	fmt.Println(floatType)
+	fmt.Println(boolType)
+	fmt.Println(floatTypeResult)
+	fmt.Println("Тип floatTypeResult:", reflect.TypeOf(floatTypeResult))
+	fmt.Println("Тип intResult:", reflect.TypeOf(intResult))
+	fmt.Println(resDevision)
+}
