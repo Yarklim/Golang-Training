26528526685cdee9776fc23636b9ca32268894d4
Yaroslav Klimas <yarklim@gmail.com>
2025-12-04 09:29:39 +0200

    ADD: cicles

---
+++ STUDY/3_cicles/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	score := 0
+
+	for i := 1; i <= 5; i++ {
+		score++
+		randomNum := rand.Intn(5)
+
+		if score == randomNum {
+			fmt.Println("Yes!")
+		} else {
+			fmt.Println("No(")
+		}
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	// Бесконечный цикл
+	for {
+		fmt.Println("Start")
+
+		if rand.Intn(5) == 1 {
+			fmt.Println("End")
+
+			break
+		}
+
+	}
+}
