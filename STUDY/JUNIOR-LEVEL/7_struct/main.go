f44ebbb804a2f1a19f9307c3c7a64985fc04266a
Yaroslav Klimas <yarklim@gmail.com>
2025-12-07 08:04:30 +0200

    UPDATE: struct

--- STUDY/JUNIOR-LEVEL/7_struct/main.go
+++ STUDY/JUNIOR-LEVEL/7_struct/main.go
@@ -2,12 +2,47 @@ package main
 
 import "fmt"
 
+// Типизация struct
 type User struct {
 	Name        string  // ""
 	Age         int     // 0
 	PhoneNumber string  // ""
 	IsClose     bool    // false
 	Rating      float64 // 0.0
+}
+
+// Конструктор
+func NewUser(
+	name string,
+	age int,
+	phoneNumber string,
+	isClose bool,
+	rating float64,
+) User {
+	if name == "" {
+		fmt.Println("Name is invalide!")
+		return User{}
+	}
+	if age <= 0 || age >= 150 {
+		fmt.Println("Age is invalide!")
+		return User{}
+	}
+	if phoneNumber == "" {
+		fmt.Println("PhoneNumber is invalide!")
+		return User{}
+	}
+	if rating < 0 || rating > 10 {
+		fmt.Println("Rating is invalide!")
+		return User{}
+	}
+
+	return User{
+		Name:        name,
+		Age:         age,
+		PhoneNumber: phoneNumber,
+		IsClose:     isClose,
+		Rating:      rating,
+	}
 }
 
 // (u User) - рессивер по значению
@@ -37,13 +72,13 @@ func (u *User) ChangeAge(age int) {
 }
 
 func main() {
-	user := User{
-		Name:        "Yar",
-		Age:         53,
-		PhoneNumber: "0678596341",
-		IsClose:     false,
-		Rating:      8.0,
-	}
+	user := NewUser(
+		"Yar",
+		53,
+		"0678596341",
+		false,
+		8.0,
+	)
 
 	user.Greeting()
 	user.ChangeRating(1.5)
