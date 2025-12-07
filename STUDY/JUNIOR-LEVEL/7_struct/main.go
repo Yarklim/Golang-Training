54708e1370502961982936aabbeefe5fa39555ec
Yaroslav Klimas <yarklim@gmail.com>
2025-12-07 07:27:36 +0200

    ADD: struct

---
+++ STUDY/JUNIOR-LEVEL/7_struct/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name        string  // ""
+	Age         int     // 0
+	PhoneNumber string  // ""
+	IsClose     bool    // false
+	Rating      float64 // 0.0
+}
+
+// (u User) - рессивер по значению
+func (u User) Greeting() {
+	fmt.Println("Hello, my name is", u.Name)
+}
+
+// (u *User) - рессивер по указателю
+func (u *User) ChangeRaiting(rating float64) {
+	ratingSum := u.Rating + rating
+	if ratingSum < 0.0 || ratingSum > 10.0 {
+		fmt.Println("Rating is invalide!")
+		return
+	}
+
+	u.Rating = ratingSum
+
+	fmt.Println("New Rating:", u.Rating)
+}
+
+func main() {
+	user := User{
+		Name:        "Yar",
+		Age:         53,
+		PhoneNumber: "0678596341",
+		IsClose:     false,
+		Rating:      8.0,
+	}
+
+	user.Greeting()
+	user.ChangeRaiting(1.5)
+
+	fmt.Println("User:", user)
+	fmt.Println("Name:", user.Name)
+	fmt.Println("Rating:", user.Rating)
+	fmt.Println("IsClose before:", user.IsClose)
+
+	user.IsClose = true
+	fmt.Println("IsClose after:", user.IsClose)
+}
