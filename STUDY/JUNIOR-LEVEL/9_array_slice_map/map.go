5502c07503da46e0116f265577320028dc08da0f
Yaroslav Klimas <yarklim@gmail.com>
2025-12-09 12:15:17 +0200

    UPDATE: map

--- STUDY/JUNIOR-LEVEL/9_array_slice_map/map.go
+++ STUDY/JUNIOR-LEVEL/9_array_slice_map/map.go
@@ -3,19 +3,21 @@ package arrayslicemap
 import "fmt"
 
 func MapArray() {
-	// weather := make(map[int]int, 10) // можно создавать как и в slice
+	// weather := make(map[string]int, 10) // можно создавать как и в slice
 
-	weather := map[int]int{
-		11: +3,
-		12: +6,
-		13: +9,
-		14: -4,
-		15: +1,
+	weather := map[string]int{
+		"Mon": +3,
+		"Tue": +6,
+		"Wed": +9,
+		"Thu": -4,
+		"Fri": +1,
+		"Sat": +1,
+		"Sun": +1,
 	}
 
-	weather[20] = +5 // добавление ключа-значения
+	weather["Tue"] = +5 // добавление/изменение ключа-значения
 
-	c, ok := weather[30] // проверка на валидность значения ключа
+	c, ok := weather["Gok"] // проверка на валидность значения ключа
 
 	// Иттерация по map (порядок элементов не сохраняется)
 	for k, v := range weather {
@@ -23,8 +25,8 @@ func MapArray() {
 		fmt.Println(k, v) // здесь копия массива!
 	}
 
-	fmt.Println(weather[12])
-	fmt.Println(weather[20])
-	fmt.Println(weather[10]) // если ключа нет, то выведет 0 (значение по умолчанию типа int)
-	fmt.Println(c, ok)       // выведет 0 false
+	fmt.Println(weather["Sat"])
+	fmt.Println(weather["Tue"])
+	fmt.Println(weather["Gok"]) // если ключа нет, то выведет 0 (значение по умолчанию типа int)
+	fmt.Println(c, ok)          // выведет 0 false
 }
