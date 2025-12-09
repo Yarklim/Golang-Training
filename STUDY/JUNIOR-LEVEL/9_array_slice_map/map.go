9413f2a4041a9c98cba11037080d86eeec5f7785
Yaroslav Klimas <yarklim@gmail.com>
2025-12-09 12:09:24 +0200

    UPDATE: map

--- STUDY/JUNIOR-LEVEL/9_array_slice_map/map.go
+++ STUDY/JUNIOR-LEVEL/9_array_slice_map/map.go
@@ -3,6 +3,8 @@ package arrayslicemap
 import "fmt"
 
 func MapArray() {
+	// weather := make(map[int]int, 10) // можно создавать как и в slice
+
 	weather := map[int]int{
 		11: +3,
 		12: +6,
@@ -11,9 +13,18 @@ func MapArray() {
 		15: +1,
 	}
 
+	weather[20] = +5 // добавление ключа-значения
+
 	c, ok := weather[30] // проверка на валидность значения ключа
 
-	fmt.Println(weather[15])
+	// Иттерация по map (порядок элементов не сохраняется)
+	for k, v := range weather {
+		weather[k] += 1
+		fmt.Println(k, v) // здесь копия массива!
+	}
+
+	fmt.Println(weather[12])
+	fmt.Println(weather[20])
 	fmt.Println(weather[10]) // если ключа нет, то выведет 0 (значение по умолчанию типа int)
 	fmt.Println(c, ok)       // выведет 0 false
 }
