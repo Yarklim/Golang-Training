6bcf1020d3c5f18e5faca38cf9e84944fea14dcd
Yaroslav Klimas <yarklim@gmail.com>
2026-01-22 13:16:27 +0200

    ADD: response body and status code

--- STUDY/REST_API/1_base/base-rest.go
+++ STUDY/REST_API/1_base/base-rest.go
@@ -29,7 +29,13 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 func payHandler(w http.ResponseWriter, r *http.Request) {
 	httpReqBody, err := io.ReadAll(r.Body) // получаю тело запроса в виде []byte
 	if err != nil {
-		fmt.Println("Fail to read HTTP body:", err)
+		w.WriteHeader(http.StatusInternalServerError) // Статус код
+		msg := "Fail to read HTTP body: " + err.Error()
+		fmt.Println(msg)
+		_, err = w.Write([]byte(msg)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 		return
 	}
 
@@ -37,7 +43,13 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 
 	paymentAmount, err := strconv.Atoi(httpReqBodyStr) // преобразую строку в число
 	if err != nil {
-		fmt.Println("Fail to convert HTTP body to integer:", err)
+		w.WriteHeader(http.StatusBadRequest) // Статус код
+		msg := "Fail to convert HTTP body to integer: " + err.Error()
+		fmt.Println(msg)
+		_, err = w.Write([]byte(msg)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 		return
 	}
 
@@ -45,7 +57,12 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 	if money-paymentAmount >= 0 {
 		money -= paymentAmount
 
-		fmt.Println("Оплата прошла успешно, остаток в кошельке:", money)
+		msg := "Оплата прошла успешно, остаток в кошельке: " + strconv.Itoa(money)
+		fmt.Println(msg)
+		_, err = w.Write([]byte(msg)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 	}
 	mtx.Unlock() // Разблокирую критическую секцию
 }
@@ -53,7 +70,13 @@ func payHandler(w http.ResponseWriter, r *http.Request) {
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	httpReqBody, err := io.ReadAll(r.Body) // получаю тело запроса в виде []byte
 	if err != nil {
-		fmt.Println("Fail to read HTTP body:", err)
+		w.WriteHeader(http.StatusInternalServerError) // Статус код
+		msg := "Fail to read HTTP body: " + err.Error()
+		fmt.Println(msg)
+		_, err = w.Write([]byte(msg)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 		return
 	}
 
@@ -61,7 +84,13 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 
 	saveAmount, err := strconv.Atoi(httpReqBodyStr) // преобразую строку в число
 	if err != nil {
-		fmt.Println("Fail to convert HTTP body to integer:", err)
+		w.WriteHeader(http.StatusBadRequest) // Статус код
+		msg := "Fail to convert HTTP body to integer: " + err.Error()
+		fmt.Println(msg)
+		_, err = w.Write([]byte(msg)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 		return
 	}
 
@@ -71,8 +100,19 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 		money -= saveAmount
 		bank += saveAmount
 
-		fmt.Println("Остаток средств в кошельке:", money)
-		fmt.Println("Сумма на счету в банке:", bank)
+		msgPay := "Остаток средств в кошельке: " + strconv.Itoa(money)
+		fmt.Println(msgPay)
+		_, err = w.Write([]byte(msgPay)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
+
+		msgBank := "Сумма на счету в банке: " + strconv.Itoa(bank)
+		fmt.Println(msgBank)
+		_, err = w.Write([]byte(msgBank)) // Тело ответа
+		if err != nil {
+			fmt.Println("Fail to write HTTP response:", err)
+		}
 	}
 	mtx.Unlock() // Разблокирую критическую секцию
 }
