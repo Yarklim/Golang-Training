d5322c88f5a86db824264466de860251a6a0b05e
Yaroslav Klimas <yarklim@gmail.com>
2026-02-28 10:43:00 +0200

    UPDATED: main.go

--- STUDY/REST_API/main.go
+++ STUDY/REST_API/main.go
@@ -1,7 +1,21 @@
 package main
 
-import base_rest "rest/1_base"
+import (
+	"fmt"
+
+	// base_rest "rest/1_base"
+	todo "rest/2_todo"
+	"rest/http"
+)
 
 func main() {
-	base_rest.BaseRest()
+	// base_rest.BaseRest()
+
+	todoList := todo.NewList()
+	httpHandlers := http.NewHTTPHandlers(todoList)
+	httpServer := http.NewHTTPServer(httpHandlers)
+
+	if err := httpServer.StartServer(); err != nil {
+		fmt.Println("failed to start http server:", err)
+	}
 }
